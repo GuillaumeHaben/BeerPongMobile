c5dde01a7951fbc02f37aaef311957132150b27e
philippe-becker <philippe.becker@telecomnancy.net>
2020-04-12 12:59:21 +0200

    Refactor Game

---
+++ components/Game.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { StyleSheet, View} from 'react-native';
+import { Button, Text, Container} from 'native-base';
+
+function Game() {
+
+    const [counterHit, updateCounterHit] = useState(0);
+    const [counterMiss, updateCounterMiss] = useState(0);
+
+    function resetCounters(){
+        updateCounterHit(0);
+        updateCounterMiss(0);
+    }
+  
+    return (
+      <Container>
+      <Button block success
+          onPress={() => {
+            updateCounterHit(counterHit+1);
+          }}>
+          <Text>HIT</Text>
+        </Button>
+        <Button block danger
+          onPress={() => {
+            updateCounterMiss(counterMiss+1);
+          }}>
+          <Text>MISS</Text>
+        </Button>
+
+        <Text>Hit = {counterHit} | Miss = {counterMiss}</Text>
+        <Text>Total = {counterHit + counterMiss}</Text>
+        <Text>{(isNaN(counterHit/(counterHit+counterMiss))) ? 0 : Math.floor(counterHit/(counterHit+counterMiss)*100)}% success</Text>
+
+        <Button block
+          onPress={() => {resetCounters()}}>
+          <Text>RESET</Text>
+        </Button>
+      </Container>
+    );
+    
+}
+
+export default Game;
--- screens/screenGame.js
+++ screens/screenGame.js
@@ -1,42 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, View} from 'react-native';
-import { Button, Text} from 'native-base';
+import Game from '../components/Game';
 
 function screenGame() {
 
-    const [counterHit, updateCounterHit] = useState(0);
-    const [counterMiss, updateCounterMiss] = useState(0);
 
-    function resetCounters(){
-        updateCounterHit(0);
-        updateCounterMiss(0);
-    }
-  
-    return (
-      <View style={styles.container}>
-       <Button full success
-          onPress={() => {
-            updateCounterHit(counterHit+1);
-          }}>
-          <Text>HIT</Text>
-        </Button>
-        <Button full danger
-          onPress={() => {
-            updateCounterMiss(counterMiss+1);
-          }}>
-          <Text>MISS</Text>
-        </Button>
-
-        <Text>Hit = {counterHit} | Miss = {counterMiss}</Text>
-        <Text>Total = {counterHit + counterMiss}</Text>
-        <Text>{(isNaN(counterHit/(counterHit+counterMiss))) ? 0 : Math.floor(counterHit/(counterHit+counterMiss)*100)}% success</Text>
-
-        <Button full
-          onPress={() => {resetCounters()}}>
-          <Text>RESET</Text>
-        </Button>
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Game></Game>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
