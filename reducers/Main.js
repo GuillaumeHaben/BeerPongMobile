3edc8130e834b37929e0e9807cd50110b79693fc
Guillaume Haben <pyxouze@gmail.com>
2020-04-12 17:07:55 +0200

    Prepare game actions

--- reducers/Main.js
+++ reducers/Main.js
@@ -1,30 +1,49 @@
 export const initialState = {
-    games: [],
-    profile: []
+    games: []
   };
   
   export const reducer = (state, action) => {
     switch (action.type) {
-      case "ADD_GAME":
-        {
-          const newId = parseInt(state.games.length)
-          const newGame = {
-            id: newId,
-            counterHit: 0,
-            counterMiss: 0
-          };
-          return { ...state,
-            games: [...state.games, newGame],
-          };
-        }
-      case "DELETE_GAME":
-        {
-          const games = Object.assign([], state.games);
-          games.splice(-1,1);
-          return { ...state,
-            games: games,
-          };
-        }
+      case "ADD_GAME": {
+        const newId = parseInt(state.games.length)
+        const newGame = {
+          id: newId,
+          counterHit: 0,
+          counterMiss: 0
+        };
+        return { ...state,
+          games: [...state.games, newGame],
+        };
+      }
+      case "UPDATE_GAME_INCREMENT": {
+        /**
+         * action.id: Game id to modify
+         * action.counter: Counter to increment, "Hit" or "Miss"
+         */
+        const gamesCopy = state.games.map((game) => {
+          if (game.id === action.id) {
+            if (action.counter == "Hit") {
+              game.counterHit = game.counterHit + 1;
+            }
+            if (action.counter == "Miss") {
+              game.counterMiss = game.counterMiss + 1;
+            }
+          }
+          return game
+        });
+        return { ...state,
+          games: gamesCopy,
+        };
+      }
+      case "DELETE_GAME": {
+        /**
+         * action.id: Game id to delete
+         */
+        const gamesCopy = state.games.filter( game => game.id !== action.id);
+        return { ...state,
+          games: gamesCopy,
+        };
+      }
       default:
         return state;
     }
--- screens/screenHome.js
+++ screens/screenHome.js
@@ -9,30 +9,24 @@ function screenHome({ navigation }) {
       <View style={styles.home}>
         <Content>
           <Button block info
-            onPress={() => {navigation.navigate('Game')}}>
+            onPress={() => { dispatch({type: "ADD_GAME"}); navigation.navigate('Game') }}>
             <Text>Start a Game</Text>
           </Button>
           <Button block primary
-            onPress={() => { dispatch({type: "ADD_GAME"}) }}>
-            <Text>Create a Game</Text>
+            onPress={() => { dispatch({type: "UPDATE_GAME_INCREMENT", id: 0, counter:"Miss"}) }}>
+            <Text>Update Game 0</Text>
           </Button>
           <Button block warning
-            onPress={() => { dispatch({type: "DELETE_GAME"}) }}>
+            onPress={() => { dispatch({type: "DELETE_GAME", id: 0}) }}>
             <Text>Delete a Game</Text>
           </Button>
           <List>
             <Text>List of games</Text>
             {state.games.map(game => (
-            <ListItem>
-              <Text>{game.id}</Text>
-            </ListItem>
-            ))}
-          </List>
-          <List>
-            <Text>List of profile</Text>
-            {state.profile.map(profile => (
-            <ListItem>
-              <Text>{profile}</Text>
+            <ListItem key={game.id} onPress={() => { navigation.navigate('Game', { id: game.id }) }}>
+              <Text>id: {game.id}</Text>
+              <Text>counterHit: {game.counterHit}</Text>
+              <Text>counterMiss: {game.counterMiss}</Text>
             </ListItem>
             ))}
           </List>
