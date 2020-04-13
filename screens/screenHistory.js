f0bede9a55e7cbd62e757bc5ddceb6f9d85cff5c
Guillaume Haben <pyxouze@gmail.com>
2020-04-13 17:37:41 +0200

    Add History

--- screens/screenHistory.js
+++ screens/screenHistory.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View} from 'react-native';
-import { Text} from 'native-base';
+import { Button, List, ListItem, Text} from 'native-base';
+import { Context } from "../context/MyContext.js"
 
 function screenHistory() {
 
+  const [state, dispatch] = useContext(Context);
+
   return (
+    state.games ? 
     <View style={styles.history}>
-      <Text>This is History</Text>
+        <Button block danger
+          onPress={() => { dispatch({type: "DELETE_GAMES"}) }}>
+          <Text>Delete All Games</Text>
+        </Button>
+        <List>
+          <Text>List of games</Text>
+          { state.games.map(game => (
+          <ListItem key={game.id} onPress={() => { navigation.navigate('Game', { id: game.id }) }}>
+            <Text>id: {game.id}</Text>
+            <Text>counterHit: {game.counterHit}</Text>
+            <Text>counterMiss: {game.counterMiss}</Text>
+          </ListItem>
+          ))}
+        </List>
+    </View>
+    :
+    <View style={styles.history}>
+      <Text>No game so far...</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    history: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'powderblue',
-    },
+  history: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 export default screenHistory;
--- screens/screenHome.js
+++ screens/screenHome.js
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 import { StyleSheet, View} from 'react-native';
-import { Button, Content, List, ListItem, Text} from 'native-base';
+import { Button, Text} from 'native-base';
 import { Context } from "../context/MyContext.js"
 
 function screenHome({ navigation }) {
@@ -9,26 +9,10 @@ function screenHome({ navigation }) {
 
   return (
     <View style={styles.home}>
-      <Content>
-        <Button block info
-          onPress={() => { navigation.navigate('Game', { id: state.games.length }) }}>
-          <Text>Start a Game</Text>
-        </Button>
-        <Button block warning
-          onPress={() => { dispatch({type: "DELETE_GAMES"}) }}>
-          <Text>Delete All Games</Text>
-        </Button>
-        <List>
-          <Text>List of games</Text>
-          { state.games.map(game => (
-          <ListItem key={game.id} onPress={() => { navigation.navigate('Game', { id: game.id }) }}>
-            <Text>id: {game.id}</Text>
-            <Text>counterHit: {game.counterHit}</Text>
-            <Text>counterMiss: {game.counterMiss}</Text>
-          </ListItem>
-          ))}
-        </List>
-      </Content>
+      <Button block info
+        onPress={() => { navigation.navigate('Game', { id: state.games.length }) }}>
+        <Text>Start a Game</Text>
+      </Button>
     </View>
   );
 }
